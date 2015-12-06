19fd11212149c8000405034e379d8afb9527a062
彭建 <pengjian@live.cn>
2015-12-06 18:58:47 +0800

    抽奖

---
+++ App22/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace App22
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int i = 0;
+        DispatcherTimer dt = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            dt.Interval = new TimeSpan(100);
+            dt.Tick += Dt_Tick;
+            dt.Start();
+        }
+
+        private void Dt_Tick(object sender, object e)
+        {
+            TB.Text = i++.ToString();  
+          
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock textblock = new TextBlock();
+            
+            textblock.Text = TB.Text;
+            textblock.HorizontalAlignment = HorizontalAlignment.Left;
+            
+            stp.Children.Add(textblock);
+            
+
+        }
+
+       
+    }
+}
